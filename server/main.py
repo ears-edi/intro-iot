95c24d5a6ac3862ae1b7a23cac8272eece32ff68
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2018-04-06 14:06:42 +0100

    Added channels and logger

--- server/main.py
+++ server/main.py
@@ -1,8 +1,11 @@
 import time
-from flask import Flask
+import json
+from flask import Flask, render_template, send_from_directory
+
+num_channels = 20
 
 app = Flask(__name__)
-ldr_data = []
+ldr_data = [[] for i in range(num_channels)]
 
 @app.route('/')
 def hello_world():
@@ -11,16 +14,29 @@ def hello_world():
     """
     return "hello"
 
-@app.route('/putldrdata/<int:data>')
-def put_ldr_data(data):
+@app.route('/putldrdata/<int:channel>/<int:data>')
+def put_ldr_data(channel, data):
     """ Stores the LDR value with the current time """
-    ldr_data.append((time.time(), data))
-    return "OK"
+    ldr_data[channel].append((time.time(), data))
+    return "OK, channel: " + str(channel) + " data: " + str(data)
 
-@app.route('/getldrdata')
-def get_ldr_data():
+@app.route('/getldrdata/<int:channel>')
+def get_ldr_data(channel):
     """ Returns all LDR data as a string """
-    return str(ldr_data)
+	if channel >= 0 and channel < num_channels:
+		return str(ldr_data[channel])
+		
+	else:
+		return "channel " + str(channel) + " out of range"
+		
+@app.route('/logger')
+def logger():
+	""" Returns html file for logger char """
+	return render_template('logger.html')
+	
+@app.route('/logger/<path:path>')
+def send_js(path):
+	return send_from_directory('templates', path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
